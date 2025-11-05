13005ddca5b156418c9cf3c4b3eecd94a942dd8f
Arvid Öman <08omaarv@taby.se>
2025-11-05 12:47:20 +0100

    snubben vill ha korv

---
+++ Glizzy.py
@@ -0,0 +1,7 @@
+antal_korvar = int(input("Hur många korvar du vill ha?"))
+
+if antal_korvar < 100:
+    print("jävla hora")
+elif antal_korvar < 1000:
+    print("Jävla Glizzy Gobbler")
+else: print("Eh, okej här då")
